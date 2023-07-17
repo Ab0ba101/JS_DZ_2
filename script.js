5a65d6982310fd771dfc6296a0ec01bd2d23529a
Ab0ba101 <timohapaskevic101@gmail.com>
2023-07-17 19:46:46 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// 1
+let cels = 10;
+let farenh = cels = 1.8 +32;
+
+console.log(farenh);
+
+// 2
+let DayMonth = 31;
+let HourMonth = DayMonth *24;
+let MinMonth = HourMonth *60;
+console.log("Hours:", HourMonth);
+console.log("Minutes:", MinMonth)
+
+// 3
+let Health = 100;
+let Energy = 100;
+
+console.log("Health:", Health - 47);
+console.log("Energy:", Energy - 39);
+
+// 4
+let Price = 1000;
+let Discount = Price *1 - 100;
+console.log("DiscountPrice:", Discount);
+
+// 5
+let Num1 = 34.678;
+console.log(Math.floor(Num1));
+
+// 6
+let Num2 = 101.876;
+console.log(parseFloat(Num2));
+
+// 7
+
+let Str = 17.76;
+console.log(parseInt(Str));
+
+// 8
+let Num3 = 87;
+console.log(Math.sqrt(Num3));
+
+// 9
+let String = 250.109;
+console.log(parseInt(String));
+console.log(toString(String));
